e064eeab22a1d53bdaa7f331e0fd795c71758b8f
macbook <macbook@macbooks-MacBook-Pro.local>
2022-09-13 10:01:25 -0300

    Import components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap";
 import React, { useState, useEffect } from "react";
 
+// Components
+import Card from "./components/Card/Card";
+import Filter from "./components/Filter/Filter";
+import Navbar from "./components/Navbar/Navbar";
+import Pagination from "./components/Pagination/Pagination";
+import Search from "./components/Search/Search";
+
 function App() {
   return (
     <div className="App">
